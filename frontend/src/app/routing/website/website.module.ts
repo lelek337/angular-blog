c0f2e2cb212e64690f3f2e93221913eeab95ee24
lelek337 <ioan.af@mail.ru>
2022-07-01 16:15:46 +0300

    add guards

---
+++ frontend/src/app/routing/website/website.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { NotFoundModule } from './routing/not-found/not-found.module';
+import { AdminModule } from './routing/admin/admin.module';
+import { AdminGuestGuard } from './guards/admin-guest.guard';
+import { AdminAuthGuard } from './guards/admin-auth.guard';
+
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        pathMatch: 'full',
+        loadChildren: () => import('./routing/home/home.module')
+        .then(module => module.HomeModule)
+      },
+      {
+        path: 'admin/auth',
+        loadChildren: () => import('./routing/admin-auth/admin-auth.module')
+        .then(module => module.AdminAuthModule),
+        canActivate: [AdminGuestGuard],
+        canLoad: [AdminGuestGuard]
+      },
+      {
+        path: 'admin',
+        loadChildren: () => import('./routing/admin/admin.module')
+        .then(module => module.AdminModule),
+        canActivate: [AdminAuthGuard],
+        canLoad: [AdminAuthGuard]
+      },
+      {
+        path: '**',
+        loadChildren: () => import('./routing/not-found/not-found.module')
+        .then(module => module.NotFoundModule)
+      }
+    ])
+  ],
+  providers: [AdminGuestGuard, AdminAuthGuard],
+})
+export class WebsiteModule { }
